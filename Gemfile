8c930073f68aa06b22293fd71873442c63cdf1ae
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2014-09-01 11:17:36 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+gem 'pg', '0.17.1'
+gem 'rails', '3.2.17'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwagDogsApi::Application.config.secret_token = '1071314b6728cb6b1209e569af30b501871f6263bfb53dd6d74ed176dc64f05cb992be24c7c58ffe586200471fb732366965c34d668e0aaf543270c75aacdf9f'
