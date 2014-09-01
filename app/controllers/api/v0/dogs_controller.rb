32bb68465af454ae9b3e09ba1a8da9a5aba8c78c
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2014-09-01 14:53:20 +0100

    swag it up

--- app/controllers/api/v0/dogs_controller.rb
+++ app/controllers/api/v0/dogs_controller.rb
@@ -2,8 +2,28 @@ module Api
   module V0
     class DogsController < Api::V0::ApiController
 
+      swagger_controller :dogs_controller, 'Dogs Controller'
+
+      swagger_api :index do
+        summary "Get the list of all dogs"
+        notes "It's possible to filter dogs"
+
+        param :query, :name, :string, :optional, "Filter by dog name"
+
+        response :ok
+      end
+
       def index
-        render json: { dogs: Dog.all }, status: :ok
+        render json: { dogs: Dog.search(params[:q]).result }, status: :ok
+      end
+
+      swagger_api :show do
+        summary "Display the dog object"
+        notes "Get and display a dog"
+
+        param :path, :id, :string, :required, "Dog id"
+
+        response :ok
       end
 
       def show
@@ -15,11 +35,84 @@ module Api
         end
       end
 
+      swagger_api :create do
+        summary "Create a dog"
+        notes "Create a dog."
+
+        param :body, :dog, :Dog, :required, "a dog object to create"
+
+        response :created
+        response :not_acceptable
+        response :unprocessable_entity
+      end
+
+      swagger_model :Dog do
+        description "Dog Object"
+        property :name, :string, :required, "Dog Name"
+        property :breed, :string, :required, "Dog breed"
+        property :color, :string, :required, "Dog color"
+      end
+
+
       def create
         return render json: {}, status: :not_acceptable unless params.has_key? 'dog'
 
         dog = Dog.new(params[:dog])
-        render json: dog, status: dog.save? ? :created : :unprocessable_entity
+        if dog.save
+          render json: dog, status: dog.valid? ? :created : :unprocessable_entity
+        else
+          render json: { dog: dog,
+                         errors: dog.errors.messages },
+                        status: dog.valid? ? :created : :unprocessable_entity
+        end
+      end
+
+      swagger_api :update do
+        summary "Update a dog"
+        notes "Update a dog."
+
+        param :path, :id, :integer, :required, "id of dog to update"
+
+        response :created
+        response :not_acceptable
+        response :unprocessable_entity
+      end
+
+      def update
+        dog = Dog.where(id: params[:id]).first
+        if dog
+          if dog.update_attributes(params[:dog])
+            render json: dog, status: dog.valid? ? :created : :unprocessable_entity
+          else
+            render json: { dog: dog,
+                           errors: dog.errors.messages },
+                          status: dog.valid? ? :created : :unprocessable_entity
+          end
+        else
+          render json: {}, status: :not_found
+        end
+      end
+
+      swagger_api :destroy do
+        summary "Delete a dog"
+        notes "Delete a dog."
+
+        param :path, :id, :integer, :required, "id of dog to delete"
+
+        response :created
+        response :not_acceptable
+        response :unprocessable_entity
+      end
+
+      def destroy
+        dog = Dog.where(id: params[:id]).first
+        if dog
+          if dog.delete
+            render json: {} ? :ok : :unprocessable_entity
+          end
+        else
+          render json: {}, status: :not_found
+        end
       end
 
     end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,5 @@
 class Dog < ActiveRecord::Base
   attr_accessible :breed, :color, :name
+
+  validates_presence_of :name, :breed
 end
